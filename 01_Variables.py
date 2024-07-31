dad2b9f05c694d518a8acf18708b1a5575319eb2
Rajpoot Himanshu <rajpoothimanshu001@gmail.com>
2024-07-31 13:00:24 +0530

    Variables

---
+++ 01_Variables.py
@@ -0,0 +1,21 @@
+# String in python
+item1 = "Food";
+item2 = "Gas";
+item3 = "Water";
+item4 = "Bike";
+print(item1, item2, item3, item4);
+
+# Numbers in python
+num1 = 10;
+num2 = 20;
+num3 = 30;
+num4 = 40;
+print(num1, num2, num3, num4);
+
+# All mathematical operation
+sum = num1 + num2;
+sub = num2-num1;
+mul = num3*num4;
+div = num4/num1;
+mod = num4%num1;
+print(f"Addition : {sum} \nSubstraction : {sub} \nMultiplication : {mul} \nDivision : {div} \nModulus : {mod}");
