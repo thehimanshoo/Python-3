452c69b1fceb01a5cab369005550aae1389d91ce
Rajpoot Himanshu <rajpoothimanshu001@gmail.com>
2024-11-19 17:56:40 +0530

    final commit

---
+++ 12_Defining_Function.py
@@ -0,0 +1,16 @@
+'''
+                                                                    Function
+                                                                   ----------
+                                                                   
+    It is also possible to define functions with a variable number of arguments. There are three forms, which can be combined.                                                               
+'''
+
+def printOddNumber(n):
+    result = []
+    
+    for num in range(1, n):
+        
+    if n % 2 == 0:
+        print("Even number")            
+        
+printOddNumber(100)
